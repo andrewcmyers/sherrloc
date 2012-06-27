21a7bcd44d937277ddf481fc4bc494324035eb42
zdfonline <9870999+zhangdf@users.noreply.github.com>
2012-06-27 23:17:57 +0000

    fixed existence path finder

--- GenErrorDiagnostic/src/constraint/graph/pathfinder/ExistancePathFinder.java
+++ GenErrorDiagnostic/src/constraint/graph/pathfinder/ExistancePathFinder.java
@@ -28,9 +28,11 @@ public class ExistancePathFinder extends CFLPathFinder {
 //		 Suppose the current reduction edge has form B<i,j>, look for
 //		 productions with form A := C B
 //		 for C<k,i> add edge A<k,j>
-		List<Edge> workingList = g.getAllEdges();
+		List<Edge> workingList = getAllReductionEdges();
 		
-		for (Edge currentedge : workingList) {
+		while ( !workingList.isEmpty()) {
+			Edge currentedge = workingList.remove(0);
+			
 			List<ReductionEdge> toAdd  = new ArrayList<ReductionEdge>();
 		
 			for (Node from : g.getAllNodes()) {
@@ -66,6 +68,50 @@ public class ExistancePathFinder extends CFLPathFinder {
 							list.addAll(((IdEdge)currentedge).getEdges());
 							newedge = new LeftEdge(
 									((LeftEdge) e).cons, from, to, list);
+						}
+	
+						if (newedge != null && !hasReductionEdge(newedge)) {
+							toAdd.add(newedge);
+						}
+					}
+				}
+			}
+			
+			// handle the case when current edge is on the left
+			
+			for (Node to : g.getAllNodes()) {
+				if (!currentedge.getFrom().equals(to)) {
+					Node from = currentedge.getFrom();
+					for (Edge e : getReductionEdges(currentedge.getTo(), to)) {
+						ReductionEdge newedge = null;
+	//					System.out.println ("testing "+from+"->"+currentedge.from+"->"+currentedge.to);
+						
+						// id := id id
+						if (e instanceof IdEdge
+								&& currentedge instanceof IdEdge) {
+							List<Edge> list = new ArrayList<Edge>();
+							list.addAll(((IdEdge)currentedge).getEdges());
+							list.addAll(((IdEdge)e).getEdges());
+							newedge = new IdEdge(from, to, list);
+						}
+						
+						// id := left right
+						else if (e instanceof RightEdge && currentedge instanceof LeftEdge 
+								&& ((LeftEdge)currentedge).cons.matches(((RightEdge)e).cons)) {
+							List<Edge> list = new ArrayList<Edge>();
+							list.addAll(((LeftEdge)currentedge).getEdges());
+							list.addAll(((RightEdge)e).getEdges());
+							newedge = new IdEdge(from, to, list);
+						}
+						
+						// left := left id
+						else if (currentedge instanceof LeftEdge
+								&& e instanceof IdEdge) {
+							List<Edge> list = new ArrayList<Edge>();
+							list.addAll(((LeftEdge)currentedge).getEdges());
+							list.addAll(((IdEdge)e).getEdges());
+							newedge = new LeftEdge(
+									((LeftEdge) currentedge).cons, from, to, list);
 						}
 	
 						if (newedge != null && !hasReductionEdge(newedge)) {
