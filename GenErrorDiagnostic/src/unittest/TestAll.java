e463d47b74e0862647487397005d103ca88ed100
zdfonline <zhang@cse.psu.edu>
2012-07-04 22:00:41 +0000

    added some unit tests

---
+++ GenErrorDiagnostic/src/unittest/TestAll.java
@@ -0,0 +1,39 @@
+package unittest;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+import constraint.graph.ConstraintGraph;
+import diagnositc.Analysis;
+
+
+public class TestAll {
+	@Test
+	public void testGraph() {
+		try {
+			/* test for Jif constraint */
+			ConstraintGraph graph = Analysis.getConstraintGraph("src/constraint/tests/jif.con", false);
+			assertEquals("jif", 1, graph.getPathNumber());
+			
+			/* test for SML constraint */
+			graph = Analysis.getConstraintGraph("src/constraint/tests/test1.con", true);
+			assertEquals("test1", 1, graph.getPathNumber());
+			
+			graph = Analysis.getConstraintGraph("src/constraint/tests/test2.con", true);
+			assertEquals("test2", 8, graph.getPathNumber());
+			
+			graph = Analysis.getConstraintGraph("src/constraint/tests/test3.con", true);
+			assertEquals("test3", 4, graph.getPathNumber());
+			
+			graph = Analysis.getConstraintGraph("src/constraint/tests/test4.con", true);
+			assertEquals("test4", 1, graph.getPathNumber());
+			
+			graph = Analysis.getConstraintGraph("src/constraint/tests/test5.con", true);
+			assertEquals("test5", 141, graph.getPathNumber());
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+}
