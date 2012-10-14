26922aecb59e825dfd211fb5d18ba0386f27e935
zdfonline <zdfonline@626dd312-8d79-473a-9c84-3bcea27b477b>
2012-10-14 22:44:26 +0000

    removing automatically generated bins, fixing an old location format

--- trunk/easyocaml-modified/ocaml-3.10.2/easyocaml/ezyEnv.ml
+++ trunk/easyocaml-modified/ocaml-3.10.2/easyocaml/ezyEnv.ml
@@ -505,11 +505,11 @@ let print_cons ppf everything env =
               true
             with Not_found -> false in 
           if not (contains s "format") then (
-            Format.fprintf ppf "@[%s %a == %a;%s\"%a\"\n@]"
+                  Format.fprintf ppf "@[%s %a[%a] == %a[];[%a]\n@]"
             (Ident.name ident)
             (List.print Ty.print " ") td.type_params
+            ExtLocation.print td.type_loc
             print_kind td.type_kind
-            "@"
             ExtLocation.print td.type_loc
           )
         )
