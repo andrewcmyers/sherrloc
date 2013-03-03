b256742a4a7b7abca5124889b0f0cc7ca538b9ae
zdfonline <zhang@cse.psu.edu>
2013-03-03 23:33:57 +0000

    code clean up

---
+++ GenErrorDiagnostic/src/diagnostic/ConsSuggestion.java
@@ -0,0 +1,58 @@
+package diagnostic;
+
+import java.util.Set;
+
+import util.HTTPUtil;
+import constraint.ast.Element;
+import constraint.graph.EquationEdge;
+
+public class ConsSuggestion implements Comparable<ConsSuggestion> {
+	int rank = 0;
+	Set<EquationEdge> edges;
+	int id;
+
+	public ConsSuggestion(int id, Set<EquationEdge> edges) {
+		this.edges = edges;
+		this.id = id;
+		for (EquationEdge edge : edges) {
+			rank += edge.getEquation().getSuccPaths();
+		}
+	}
+
+	@Override
+	public int compareTo(ConsSuggestion o) {
+		return new Integer(rank).compareTo(o.rank);
+	}
+
+	public String toHTML() {
+		StringBuffer sb = new StringBuffer();
+		// sb.append("<LI>\n");
+		sb.append("<span class=rank>(rank " + rank + ")</span> ");
+		for (EquationEdge c : edges) {
+			StringBuffer locBuffer = new StringBuffer();
+			Element left = c.getEquation().getFirstElement();
+			locBuffer.append("['left', \'" + left.getPosition().toString()
+					+ "\'],");
+			Element right = c.getEquation().getSecondElement();
+			locBuffer.append("['right', \'" + right.getPosition().toString()
+					+ "\']");
+			String loc = locBuffer.toString();
+			sb.append("<span class=\"mincut\" ");
+			HTTPUtil.setShowHideActions(true, sb, loc, id);
+			sb.append(">");
+			sb.append("<code id=\"left" + id + "\">" + left.toDetailString()
+					+ "</code>");
+			sb.append(" has the same type as ");
+			sb.append("<code id=\"right" + id + "\">" + right.toDetailString()
+					+ "</code></span>");
+			sb
+					.append("<button onclick=\"hide_all();show_elements_perm(false, [");
+			sb.append(loc);
+			sb.append("]); ");
+			sb.append("show_cut_perm(" + id + ")\" ");
+			// setShowHideActions(false, sb, loc, id);
+			sb.append(">show it</button><br>\n");
+		}
+		return sb.toString();
+	}
+}
