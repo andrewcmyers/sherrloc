f4b8af4c3bdd02cfad4bddfa8d69e7c52a841d16
zdfonline <9870999+zhangdf@users.noreply.github.com>
2013-03-13 05:07:15 +0000

    utf8 encodings

--- GenErrorDiagnostic/src/constraint/graph/ConstraintPath.java
+++ GenErrorDiagnostic/src/constraint/graph/ConstraintPath.java
@@ -200,12 +200,12 @@ public class ConstraintPath {
 		ret += "\n----Start of one path----\n";
 		ElementNode leftmost = (ElementNode) getFirst();
 //		leftmost.setCause();
-		ret += leftmost.getElement().toDotString()+"\n";
+		ret += leftmost.getElement().toString()+"\n";
 		for (int k = 0; k < size(); k++) {
 			Edge edge = edges.get(k);
 			ret += "--> (" + (edge.toString()) + ")\n";
 //			if (finder.getPath(leftmost, edge.to)!=null)
-				ret += ((ElementNode)edge.to).getElement().toDotString()+"\n";
+				ret += ((ElementNode)edge.to).getElement().toString()+"\n";
 		}
 		ret += "----End of one path----\n";
 		return ret;
