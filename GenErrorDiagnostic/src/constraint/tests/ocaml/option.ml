87171fb263535f6482d98bb45d8eb4651d72bf37
zdfonline <zhang@cse.psu.edu>
2013-12-23 04:18:30 +0000

    useless tests, changed symbols of bottom and top

--- GenErrorDiagnostic/src/constraint/tests/ocaml/option.ml
+++
@@ -1,6 +0,0 @@
-let func data = 
-  match data with
-   | None -> ""
-   | Some id -> id + 1 in
-func None;
-func "abc"
--- GenErrorDiagnostic/src/constraint/tests/ocaml/test1.ml
+++
@@ -1,5 +0,0 @@
-module Foo = struct
-
-   let x = sqrt(2.0) in x ^ "hello"
-
-end
