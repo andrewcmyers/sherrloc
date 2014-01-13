6330d187943702c137b6c084b0955f0ab0de26a9
zdfonline <zdfonline@626dd312-8d79-473a-9c84-3bcea27b477b>
2014-01-13 20:27:33 +0000

    updated scripts and deleted unused files

--- trunk/GenErrorDiagnostic/test/friendmap/compile_fm.sh
+++ trunk/GenErrorDiagnostic/test/friendmap/compile_fm.sh
@@ -1,5 +1,13 @@
 #!/bin/sh
 FMHOME='/home/zhdf/APL/Fabric/examples/friendmap'
+FABHOME="/home/zhdf/APL/Fabric/";
+
+# first, revert Fabric to the version where all examples compiles
+#cd $FABHOME
+#svn up -r{20120306}
+#ant clobber
+#ant
+
 cd $FMHOME
 # update to the reversion specified by the command line
 svn up -r $@
