824cba824d6a02dbc3a1de48e00d430810449a33
zdfonline <zhang@cse.psu.edu>
2014-02-07 19:09:06 +0000

    missing files

---
+++ GenErrorDiagnostic/src/diagnostic/ExprInfer.java
@@ -0,0 +1,51 @@
+package diagnostic;
+
+import graph.ConstraintPath;
+import graph.ElementNode;
+import graph.Node;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import constraint.ast.Element;
+import util.EntityExplanationFinder;
+import util.HeuristicSearch;
+import diagnostic.explanation.Entity;
+import diagnostic.explanation.ExprEntity;
+
+public class ExprInfer extends InferenceEngine {
+	Map<String, Integer> succCount;
+	
+	public ExprInfer(UnsatPaths paths, Map<String, Integer> succCount) {
+		super(paths);
+		this.succCount = succCount;
+	}
+	
+	@Override
+	public Set<Entity> getCandidates() {
+    	Set<Entity> cand = new HashSet<Entity>();
+		
+    	for (ConstraintPath path : paths.errPaths) {
+    		for (Node n : path.getAllNodes()) {
+    			Element e = ((ElementNode)n).getElement();
+    			if (!e.getPosition().isEmpty())
+    				cand.add(new ExprEntity(n.toString(), e.toSnippetString(), 
+    						e.getPosition().toString(), succCount.get(n.toString())));
+    		}
+    	}
+    	
+    	return cand;
+	}
+	
+	@Override
+	public HeuristicSearch getAlogithm(Set<Entity> candidates) {
+    	Entity[] candarr = candidates.toArray(new Entity[candidates.size()]);
+		return new EntityExplanationFinder(paths, candarr);
+	}
+	
+	@Override
+	public String HTMLinfo() {
+		return "<H4>Expressions in the source code that appear most likely to be wrong (mouse over to highlight code):</H4>\n";
+	}	
+}
---
+++ GenErrorDiagnostic/src/diagnostic/InferenceEngine.java
@@ -0,0 +1,88 @@
+package diagnostic;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+
+import util.HeuristicSearch;
+import diagnostic.explanation.Entity;
+import diagnostic.explanation.Explanation;
+
+/**
+ * Super class of all explanation inference algorithms.
+ */
+public abstract class InferenceEngine {
+	
+	protected UnsatPaths paths;
+	
+	/**
+	 * @param paths A set of unsatisfiable paths (errors) to be explained
+	 */
+	public InferenceEngine(UnsatPaths paths) {
+		this.paths = paths;
+	}
+	
+	/**
+	 * @return Best explanation of the errors observed in constraint graph
+	 */
+	public String infer (boolean isConsole, boolean verbose) {
+    	final Set<Entity> cand = getCandidates();
+    	HeuristicSearch algorithm = getAlogithm(cand);
+    	
+    	StringBuffer sb = new StringBuffer();
+		long startTime = System.currentTimeMillis();
+		Set<Explanation> results = algorithm.AStarSearch();
+		long endTime =  System.currentTimeMillis();
+		if (verbose)
+			System.out.println("ranking_time: "+(endTime-startTime));
+		
+		if (!isConsole)
+			sb.append(HTMLinfo());
+
+		List<Explanation> cuts = new ArrayList<Explanation>();
+		for (Explanation set : results) {
+			cuts.add(set);
+		}
+		Collections.sort(cuts);
+
+		double best=Double.MAX_VALUE;
+		int i=0;
+		for ( ; i<cuts.size(); i++) {
+			if (cuts.get(i).getWeight()>best)
+				break;
+			best = cuts.get(i).getWeight();
+			if (isConsole)
+				sb.append(cuts.get(i).toConsole()+"\n");
+			else
+				sb.append(cuts.get(i).toHTML());
+		}
+		if (verbose)
+			System.out.println("top_rank_size: "+i);
+		if (!isConsole) {
+			sb.append("<button onclick=\"show_more_expr()\">show/hide more</button><br>\n");
+			sb.append("<div id=\"more_expr\">");
+			for (; i < cuts.size(); i++) {
+				sb.append(cuts.get(i).toHTML());
+			}
+			sb.append("</div>\n");
+		}
+		return sb.toString();
+	}
+	
+	/**
+	 * @return A set of entities that the inference is performed on
+	 */
+	public abstract Set<Entity> getCandidates();
+	
+	/**
+	 * @param candidates Basic elements of possible explanations
+	 * @return Return an instance of heuristic search algorithm to use
+	 */
+	public abstract HeuristicSearch getAlogithm (Set<Entity> candidates);
+	
+	/**
+	 * @return A string describing the nature of returned entities in HTML format
+	 */
+	public abstract String HTMLinfo ();	
+}
