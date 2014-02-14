7f15c3d78411bd4f2431aaa622aa29721a1871d9
zdfonline <zdfonline@626dd312-8d79-473a-9c84-3bcea27b477b>
2014-02-14 18:02:15 +0000

    more

--- trunk/GenErrorDiagnostic/src/constraint/ast/Relation.java
+++ trunk/GenErrorDiagnostic/src/constraint/ast/Relation.java
@@ -1,13 +1,17 @@
 package constraint.ast;
 
+/**
+ * Relation on constraint elements. It can either be <= (LEQ) or == (EQ)
+ */
 public enum Relation {
 	LEQ, EQ;
-	
+
+	@Override
 	public String toString() {
 		String s = super.toString();
-		if (s.equals("LEQ")) 
+		if (s.equals("LEQ"))
 			return "<=";
-		else 
+		else
 			return "==";
 	}
 };
