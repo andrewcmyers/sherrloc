122b28219e8f860260cdb721dfc924d0b89cd139
zdfonline <zdfonline@626dd312-8d79-473a-9c84-3bcea27b477b>
2014-02-14 21:33:43 +0000

    extract functionality of constraint analysis to a new class

--- trunk/GenErrorDiagnostic/src/diagnostic/Analysis.java
+++ trunk/GenErrorDiagnostic/src/diagnostic/Analysis.java
@@ -1,9 +1,6 @@
 package diagnostic;
 
 import graph.ConstraintGraph;
-import graph.ConstraintPath;
-import graph.Edge;
-import graph.Node;
 
 import java.io.BufferedWriter;
 import java.io.FileInputStream;
@@ -11,21 +8,10 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 
 import util.HTTPUtil;
 import util.PrettyPrinter;
-import constraint.analysis.PathFinder;
-import constraint.analysis.ShortestPathFinder;
-import constraint.ast.ConstructorApplication;
-import constraint.ast.Element;
-import constraint.ast.Hypothesis;
-import constraint.ast.JoinElement;
-import constraint.ast.MeetElement;
+import constraint.analysis.ConstraintAnalysis;
 import constraint.parse.GrmLexer;
 import constraint.parse.parser;
 
@@ -35,20 +21,16 @@ import constraint.parse.parser;
 public class Analysis implements PrettyPrinter {
 	private DiagnosticOptions option;
 	private ConstraintGraph graph;	// a constraint graph from constraints
-	private UnsatPaths unsatPaths;	// a set of unsatisfiable paths from constraint analysis on graph
-
+	private ConstraintAnalysis cana;
+	
 	/** internal states */
-	private HashMap<Hypothesis, Hypothesis> cachedEnv;	// Reuse saturated hypothesis graph when possible
-    private boolean DEBUG = false;    
-    private boolean done = false;	
 	HTTPUtil util;
 	
 	private Analysis(ConstraintGraph g, DiagnosticOptions option) {
 		graph = g;
 		this.option = option;
-        unsatPaths = new UnsatPaths();
         util = new HTTPUtil();
-        cachedEnv = new HashMap<Hypothesis, Hypothesis>();
+        cana = new ConstraintAnalysis(graph, option.isSymmetric(), option.isVerbose(), option.isRecursive());
 	}
 
 	/**
@@ -82,123 +64,25 @@ public class Analysis implements PrettyPrinter {
 	    return getAnalysisInstance(option);
 	}
 	
-	/**
-	 * Return an instance of constraint analysis. Currently, the only analysis
-	 * implemented is {@link ShortestPathFinder}
-	 * 
-	 * @return An constraint analysis algorithm
-	 */
-	public PathFinder getPathFinder () {
-		return new ShortestPathFinder(graph);
+	public int getPathNumber() {
+	 	return cana.getPathNumber();
 	}
-	
-    public void genErrorPaths ( ) {        
-        ArrayList<Node> startNodes = new ArrayList<Node>();
-        ArrayList<Node> endNodes = new ArrayList<Node>();
-        Set<Element> elements = graph.getAllElements();
-        Set<Element> testElements = new HashSet<Element>();
-                
-        if (DEBUG) {
-        	System.out.println("Total nodes before path generaton: " + elements.size());        
-        }
-        
-        for (Element element : elements) {
-            if (!(element instanceof JoinElement))                    
-            	startNodes.add(graph.getNode(element));
-			if (!(element instanceof MeetElement))
-            	endNodes.add(graph.getNode(element));
-        }
-        
-        if (DEBUG) {
-        	System.out.println("Total start nodes before path generaton: " + startNodes.size());
-        	System.out.println("Total end nodes before path generaton: " + endNodes.size());
-        	System.out.println("Total comparison required: " + startNodes.size() * endNodes.size());
-        }
-
-    	PathFinder finder = getPathFinder();
-    	if (option.isVerbose())
-    		System.out.println("graph_size: "+graph.getAllNodes().size());
-				
-		for (Node start : startNodes) {
-			for (Node end : endNodes) {
-				Element e1 = start.getElement();
-				Element e2 = end.getElement();
-				
-				if (option.isSymmetric() && (start.getIndex() <= end.getIndex()))
-						continue;
-				
-				List<Edge> l = finder.getPath(start, end, option.isVerbose());
-				if (l==null) continue;
-								
-				if (!option.isRecursive() && start.getIndex() != end.getIndex()) {
-					if ( (e1 instanceof ConstructorApplication && e1.getVars().contains(e2)) 
-					  || (e2 instanceof ConstructorApplication && e2.getVars().contains(e1))) {
-						ConstraintPath path = new ConstraintPath(l, finder, graph.getEnv(), cachedEnv);
-						path.setCause();
-						unsatPaths.addUnsatPath(path);
-						continue;
-					}
-				}
-				
-				// if one end is variable, or an join/meet with a variable, the satisfiability is trivial
-				if (e1.trivialEnd() || e2.trivialEnd()) {
-					continue;
-				}
-								
-				// less interesting paths
-				if (e1.isBottom() || e2.isTop())
-					continue;
-								
-				ConstraintPath path = new ConstraintPath(l, finder, graph.getEnv(), cachedEnv);
-
-				if (path.isSuccPath()) {
-					path.incSuccCounter();
-					continue;
-				}								
-				else if (path.isUnsatPath()) {
-					testElements.add(e1);
-					testElements.add(e2);
-					path.setCause();
-					unsatPaths.addUnsatPath(path);
-					if (DEBUG)
-						System.out.println(path);
-				}
-			}
-		}
-				
-		done = true;
-	}
-    
-    public int getPathNumber () {
-    	if (!done) {
-    		genErrorPaths();
-    	}
-    	int ret = unsatPaths.size();
-    	return ret;
-    }
-    
+	       
     public int getAssumptionNumber () {
-    	if (!done) {
-    		genErrorPaths();
-    	}
-    	return (new MissingHypoInfer(unsatPaths)).getAssumptionNumber();
+    	UnsatPaths paths = cana.genErrorPaths();
+    	return (new MissingHypoInfer(paths)).getAssumptionNumber();
     }
     
     public String getAssumptionString () {
-    	if (!done) {
-    		genErrorPaths();
-    	}
-    	return (new MissingHypoInfer(unsatPaths)).getAssumptionString();
+    	UnsatPaths paths = cana.genErrorPaths();
+    	return (new MissingHypoInfer(paths)).getAssumptionString();
     }
         
     private void writeToDotFile () {
         String filename;
 
         filename = "error.dot";
-        
-        if (!done) 
-        	genErrorPaths();
-        
+
         try {
             FileOutputStream fstream = new FileOutputStream(filename);
             OutputStreamWriter out = new OutputStreamWriter(fstream,"UTF-8");
@@ -224,8 +108,7 @@ public class Analysis implements PrettyPrinter {
     public String toHTMLString() {
     	StringBuffer sb = new StringBuffer();
     
-        if (!done) 
-        	genErrorPaths();
+		UnsatPaths paths = cana.genErrorPaths();
         
         // out.write(getHeader());
         sb.append(HTTPUtil.getFeedback());
@@ -237,7 +120,7 @@ public class Analysis implements PrettyPrinter {
     			"<HR>\n");
     	
     	// type check succeeded
-    	if (unsatPaths.size()==0) {
+    	if (paths.size()==0) {
     		sb.append("<H2>");
 			sb.append("The program passed type checking. No errors were found.</H2>");
             sb.append("<script type=\"text/javascript\">"+
@@ -245,8 +128,8 @@ public class Analysis implements PrettyPrinter {
     	}
     	else {
    			sb.append("<H2>One typing error is identified<H2>");
-    		sb.append(getOneSuggestion(option.sourceName, unsatPaths));
-    		sb.append(util.genAnnotatedCode(unsatPaths, option.sourceName) +
+    		sb.append(getOneSuggestion(option.sourceName, paths));
+    		sb.append(util.genAnnotatedCode(paths, option.sourceName) +
     				(option.sourceName.contains("jif")?("<script>colorize_all(); numberSuggestions();</script>\n")
     				:("<script>numberSuggestions();</script>\n")));
         }
@@ -256,14 +139,13 @@ public class Analysis implements PrettyPrinter {
                 
     @Override
     public String toConsoleString () {
-        if (!done) 
-        	genErrorPaths();
-        
+		UnsatPaths paths = cana.genErrorPaths();
+
         // type check succeeded
-        if (unsatPaths.size()==0) {
+        if (paths.size()==0) {
 			return ("The program passed type checking. No errors were found.");
 		} else {
-			return ("One typing error is identified\n" + getOneSuggestion(option.sourceName, unsatPaths));
+			return ("One typing error is identified\n" + getOneSuggestion(option.sourceName, paths));
 		}
     }
     
