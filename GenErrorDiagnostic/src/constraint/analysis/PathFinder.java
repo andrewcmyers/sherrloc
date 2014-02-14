b91dbcd0bfea5e803f6e9f2978831f648bdb792b
zdfonline <zhang@cse.psu.edu>
2014-02-14 22:47:32 +0000

    more edits

--- GenErrorDiagnostic/src/constraint/analysis/PathFinder.java
+++ GenErrorDiagnostic/src/constraint/analysis/PathFinder.java
@@ -1,37 +1,30 @@
 package constraint.analysis;
 
-import graph.ConstraintGraph;
 import graph.Edge;
 import graph.Node;
 
 import java.util.List;
 
 /**
- * A path finder is used to saturate a constraint graph
+ * The interface for path finders
  */
-public abstract class PathFinder {
-	protected boolean initialized = false;
-	protected final ConstraintGraph g;
+public interface PathFinder {
 
-	public PathFinder(ConstraintGraph graph) {
-		g = graph;
-	}
-
-	abstract void initialize();
-
-	abstract protected List<Edge> _getPath(Node start, Node end);
-
-	public List<Edge> getPath(Node start, Node end, boolean verbose) {
-		if (!initialized) {
-			long startTime = System.currentTimeMillis();
-			initialize();
-			initialized = true;
-			long endTime = System.currentTimeMillis();
-			if (verbose)
-				System.out.println("path_finding time: "
-						+ (endTime - startTime));
-		}
-
-		return _getPath(start, end);
-	}
+	/**
+	 * Return a path in the constraint graph, when a partial ordering on
+	 * <code>start</code> and <code>end</code> is derivable from all constraints
+	 * along the returned path
+	 * 
+	 * @param start
+	 *            Node on LHS
+	 * @param end
+	 *            Node on RHS
+	 * @param verbose
+	 *            True when collecting data for evaluation
+	 * 
+	 * @return A constraint path such that <code>start <= end</code> is
+	 *         derivable from constraints along the path. Return null if no such
+	 *         path exists
+	 */
+	public List<Edge> getPath(Node start, Node end, boolean verbose);
 }
