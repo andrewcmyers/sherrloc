791f7b77b744efaedfe0644fefb55c1d414357aa
zdfonline <9870999+zhangdf@users.noreply.github.com>
2014-02-19 17:02:47 +0000

    reorganizing packages

--- GenErrorDiagnostic/src/diagnostic/InferenceEngine.java
+++ GenErrorDiagnostic/src/diagnostic/InferenceEngine.java
@@ -5,7 +5,6 @@ import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 
-import util.HeuristicSearch;
 import diagnostic.explanation.Entity;
 import diagnostic.explanation.Explanation;
 
