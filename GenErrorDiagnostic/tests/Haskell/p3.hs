83154f31d20ff1fe245d76953259eabfc8aa0a58
zdfonline <9870999+zhangdf@users.noreply.github.com>
2014-06-03 04:10:03 +0000

    simple Haskell examples, and a script that translates GHC constraints to SHErrLoc

---
+++ GenErrorDiagnostic/tests/Haskell/p3.hs
@@ -0,0 +1,15 @@
+
+{-# LANGUAGE OverlappingInstances, FlexibleInstances #-}
+-- tcfail121.hs
+
+module ShouldFail where
+
+class Foo a where
+  op :: a -> a
+
+instance Foo a => Foo [a] 
+instance Foo [Int]
+
+foo :: Foo a => [a] -> [a]
+foo x = op x
+-- Correct instance depends on instantiation of 'a' 
