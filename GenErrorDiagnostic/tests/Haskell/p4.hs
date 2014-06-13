9783adef903717499d14ffe9f4f57648e00a0775
zdfonline <9870999+zhangdf@users.noreply.github.com>
2014-06-13 19:13:33 +0000

    a more complicated example; need to add assumptions so that SHErrLoc can avoid false positives

---
+++ GenErrorDiagnostic/tests/Haskell/p4.hs
@@ -0,0 +1,30 @@
+
+
+-- Ambiguity error reporting
+-- tcfail128.hs
+
+module Main where
+
+import Data.Array.MArray (thaw,freeze)
+import Data.Array.Unboxed as UA (UArray,listArray)
+import Data.Array.IArray as IA (Array,listArray)
+
+main :: IO ()
+main = do let sL = [1,4,6,3,2,5]
+	      dim = length sL
+	      help :: [FlatVector]
+	      help = [listFlatVector (1,s) [0|i<-[1..s]]|s<-sL]   
+	      tmp :: Vector FlatVector 
+	      tmp = listVector (1,dim) help
+	  v <- thaw tmp
+	  return ()
+
+type FlatVector  = UArray Int Double
+
+listFlatVector :: (Int,Int) -> [Double] -> FlatVector
+listFlatVector = UA.listArray
+
+type Vector a = Array Int a
+
+listVector :: (Int,Int) -> [a] -> Vector a
+listVector = IA.listArray
