103137318a675ebeaef2f6ff7cb0c9e6cefbd454
zdfonline <9870999+zhangdf@users.noreply.github.com>
2014-07-06 18:37:33 +0000

    combined file with error location annotations

---
+++ GenErrorDiagnostic/tests/Haskell/Chen/combine_files.pl
@@ -0,0 +1,25 @@
+#!/usr/bin/perl
+
+print "Start combining (annotated) separate files for all examples\n";
+
+my $examples = "AllExamples.hs";	
+
+open (FILE, ">$examples") or die "$examples does not exist!";
+
+my $count = 1;
+while ($count <= 119) {
+        open (OUT, "<p$count.hs") or die "p$count.hs does not exist!";
+
+	while (<OUT>) {
+		$line = $_;
+		if ($line !~ "module Example where" and $line !~ "import Data.Char") {
+			print FILE $line;
+		}
+	}	
+	close OUT;
+	$count ++;
+}
+
+close FILE;
+
+print "done!\n";
