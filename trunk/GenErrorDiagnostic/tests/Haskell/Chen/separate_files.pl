7ce4b07ab3f8f8047753402fd8ce3f7b6dee96df
zdfonline <zdfonline@626dd312-8d79-473a-9c84-3bcea27b477b>
2014-07-07 17:25:24 +0000

    generate examples with type classes

--- trunk/GenErrorDiagnostic/tests/Haskell/Chen/separate_files.pl
+++ trunk/GenErrorDiagnostic/tests/Haskell/Chen/separate_files.pl
@@ -3,34 +3,41 @@
 print "Start generating separate files for all examples\n";
 
 my $examples = "AllExamples.hs";	
+my $classexamples = "Class.hs";	
 
-open (FILE, "<$examples") or die "$examples does not exist!";
-
-my $count = 0;
-my $content="";
-while (<FILE>) {
-	if (/-- Problem:/) {
-		if ($count != 0) {
-			open (OUT, ">p$count.hs");
-			print OUT "module Example where\n\n";
-			print OUT "import Data.Char\n\n";
-			print OUT $content;
-			close OUT;
-			$content = "";
-			$count ++;
-		}
-		else {
-			$content = ""; 
-			$count ++;
-		}
-	}	
-	$content .= $_;
+sub separate_one_file {
+	$filename = shift;
+	$prefix = shift;
+	my $count = 0;
+	open (FILE, "<$filename") or die "$filename does not exist!";
+	my $content="";
+	while (<FILE>) {
+		if (/-- Problem:/) {
+			if ($count != 0) {
+				open (OUT, ">$prefix$count.hs");
+				print OUT "module Example where\n\n";
+				print OUT "import Data.Char\n\n";
+				print OUT $content;
+				close OUT;
+				$content = "";
+				$count ++;
+			}
+			else {
+				$content = ""; 
+				$count ++;
+			}
+		}	
+		$content .= $_;
+	}
+	open (OUT, ">$prefix$count.hs");
+	print OUT "module Example where\n\n";
+	print OUT "import Data.Char\n\n";
+	print OUT $content;
+	$count ++;
+	close OUT;
 }
 
-open (OUT, ">p$count.hs");
-print OUT "module Example where\n\n";
-print OUT "import Data.Char\n\n";
-print OUT $content;
-close OUT;
+separate_one_file ($examples, "p");
+separate_one_file ($classexamples, "s");
 
 print "done!\n";
