ffc2d8cf400f119219925f341260a7fa568c9c3d
zdfonline <zdfonline@626dd312-8d79-473a-9c84-3bcea27b477b>
2014-07-07 20:29:52 +0000

    small edits

--- trunk/GenErrorDiagnostic/tests/Haskell/gen_constraints.pl
+++ trunk/GenErrorDiagnostic/tests/Haskell/gen_constraints.pl
@@ -2,9 +2,9 @@
 
 print "Start generating constraints for all examples\n";
 
-$ghchome="/home/zhdf/workspace/GHC-7.8.2-modified/ghc-7.8.2/bin/";
-$ghc = $ghchome."ghc";
-$opt="-ddump-tc-trace -XMonoLocalBinds";
+$ghchome="../../../GHC-7.8.2-modified/ghc-7.8.2/inplace/bin/";
+$ghc = $ghchome."ghc-stage1";
+$opt="-ddump-tc-trace -fno-code -c";
 
 # the map from file name to generated graph name
 # this is required since one file may generate 
@@ -12,12 +12,10 @@ $opt="-ddump-tc-trace -XMonoLocalBinds";
 
 # the following files with util files in reversion 3141
 @progs = (
-	"p1",
-	"p2",
-	"p3",
+	"family",
+	"overlapping",
+	"unsoundness",
 	"p4",
-	"p5",
-	"p6",
 );
 
 foreach (@progs) {
