9289bd070f409071e5401535fa16622d727481de
zdfonline <zhang@cse.psu.edu>
2014-07-08 20:00:19 +0000

    examples with type classes

---
+++ GenErrorDiagnostic/tests/Haskell/Chen/s3.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+module Example where
+
+import Data.Char
+
+-- Problem: 'a' and True should have the same type
+class Collects ce e | ce -> e where
+    empty :: ce
+    insert :: e -> ce -> ce
+    
+f c = insert 'a' (insert True c)
+
+-- 12,14-16  12,26-29
---
+++ GenErrorDiagnostic/tests/Haskell/Chen/s5.hs
@@ -0,0 +1,15 @@
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE FunctionalDependencies #-}
+module Example where
+
+import Data.Char
+
+-- Problem: empty's type is always ambiguous
+class Collects c e where
+    insert :: e -> c -> c
+    lookup :: c -> Int -> Maybe e
+    empty :: c
+    
+init e = insert e empty
+
+-- 13,19-23 
