70214f190cb9f3dc6a3f66547faa3e70a0993b25
zdfonline <zdfonline@626dd312-8d79-473a-9c84-3bcea27b477b>
2014-07-12 21:25:54 +0000

    missing files

---
+++ trunk/GenErrorDiagnostic/src/sherrloc/constraint/ast/FunctionApplication.java
@@ -0,0 +1,106 @@
+package sherrloc.constraint.ast;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import sherrloc.graph.Variance;
+
+/**
+ * This class represents an application of a {@link Function}
+ */
+public class FunctionApplication extends Application {
+	private final Function func;
+	
+	/**
+	 * @param cons A constructor
+	 * @param elements Parameters applied to the constructor <code>cons</code>
+	 */
+	public FunctionApplication(Function f, List<Element> elements) {
+		super("", elements);
+		this.func = f;
+	}
+	
+	/**
+	 * @return Function being applied to
+	 */
+	public Function getFunc() {
+		return func;
+	}
+	
+	@Override
+	String getSymbol() {
+		return func.toString();
+	}
+	
+	@Override
+	public boolean equals(Object o) {
+		if (o instanceof FunctionApplication) {
+			FunctionApplication fe = (FunctionApplication) o;
+			if (pos.equals(fe.pos) && func.equals(fe.func)) {
+				if (fe.getElements().size() == elements.size()) {
+					for (int i = 0; i < elements.size(); i++) {
+						if (!elements.get(i).equals(fe.getElements().get(i)))
+							return false;
+					}
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	@Override
+	public int hashCode() {
+		int ret = func.hashCode()*233;
+		for (Element e : elements) {
+			ret += e.hashCode()*17;
+		}
+		return ret+pos.hashCode();
+	}
+	
+	@Override
+	public boolean isTop() {
+		return false;
+	}
+	
+	@Override
+	public boolean isBottom() {
+		return false;
+	}
+	
+	@Override
+	public boolean trivialEnd() {
+		return false;
+	}
+		
+	@Override
+	public Element getBaseElement() {
+		List<Element> baseElements =  new ArrayList<Element>();
+		for (Element e : elements) {
+			baseElements.add(e.getBaseElement());
+		}
+		return new FunctionApplication((Function)func.getBaseElement(), baseElements) ;
+	}
+	
+	@Override
+	public Variance getVariance() {
+		return Variance.NONE;
+	}
+	
+	@Override
+	public boolean unifyWith(Element e, Map<QuantifiedVariable, Element> map) {
+		if (e instanceof FunctionApplication) {
+			FunctionApplication fa = (FunctionApplication) e;
+			if (!func.unifyWith(fa.func, map))
+				return false;
+			return super.unifyWith(e, map);
+		}
+		return false;
+	}
+	
+	@Override
+	public Element subst(Map<QuantifiedVariable, Element> map) {
+		return new FunctionApplication(func, substElements(map));
+	}
+}
