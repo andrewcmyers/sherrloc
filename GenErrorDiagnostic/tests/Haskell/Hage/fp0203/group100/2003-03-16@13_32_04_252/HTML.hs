171bf1f9cd1ebd0fdcf87f2e5983a80705049940
zdfonline <zhang@cse.psu.edu>
2014-07-24 22:36:11 +0000

    added a few examples

---
+++ GenErrorDiagnostic/tests/Haskell/Hage/fp0203/group100/2003-03-16@13_32_04_252/HTML.hs
@@ -0,0 +1,32 @@
+module HTML where
+
+
+data HTML = Tekst String
+          | EnkelTag Tag [Attribuut]
+          | DubbelTag Tag [Attribuut] [HTML]
+
+type Tag = String
+
+
+type Attribuut = (Naam, Waarde)
+
+type Naam = String
+type Waarde = String
+
+
+
+
+
+
+type Requirement = (String, String)
+test (DubbelTag "LI" _ (x:xs)) | (vergelijk x False) || (test DubbelTag "LI" [] xs) =  False
+
+
+
+
+
+
+vergelijk (DubbelTag "UL" _ _) False = True
+
+-- () is missing for "DubbelTag "LI" [] xs"
+-- 22,63-82
