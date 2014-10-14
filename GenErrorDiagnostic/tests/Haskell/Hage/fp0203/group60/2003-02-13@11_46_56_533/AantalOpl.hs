2b652fce418e1dacbc7e79063cf0c5793192a106
zdfonline <zhang@cse.psu.edu>
2014-10-14 23:05:32 +0000

    fixed a few locations for Helium; added a script to show percentage

--- GenErrorDiagnostic/tests/Haskell/Hage/fp0203/group60/2003-02-13@11_46_56_533/AantalOpl.hs
+++ GenErrorDiagnostic/tests/Haskell/Hage/fp0203/group60/2003-02-13@11_46_56_533/AantalOpl.hs
@@ -4,4 +4,4 @@ aantalOpl a b c = ((- b + sqrt (b * b - 4.0 * a * c)) / ( 2.0 * a ))
                   ((- b - sqrt (b * b - 4.0 * a * c)) / ( 2.0 * a ))
 
 -- sig should be Float -> Float -> Float -> [Float]; the expression should be [..., ...]
--- 2,1-9   2,14-54   2,41-54   3,19-4,68    3,20-67
+-- 2,1-9   2,14-54   2,41-54   3,19-4,68    3,20-67    3,20-76
