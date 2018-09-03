154900f92ae74b2aee09a0d6679b37daa9b08cc7
Cosku Acay <coskuacay@gmail.com>
2018-09-03 17:55:36 -0400

    ghc: cleanup repo and fix dependencies   - rename top level directory   - untrack ignored files   - track (some) dependencies using git submodules   - modify sync-all get so it works with our setup   - modify sync-all so it checks out the correct version of dependencies (it uses master by default)   - update build instructions

--- GHC-7.8.2-modified/ghc-7.8.2/libraries/integer-simple/ghc.mk
+++
@@ -1,5 +0,0 @@
-libraries/integer-simple_PACKAGE = integer-simple
-libraries/integer-simple_dist-install_GROUP = libraries
-$(if $(filter integer-simple,$(PACKAGES_STAGE0)),$(eval $(call build-package,libraries/integer-simple,dist-boot,0)))
-$(if $(filter integer-simple,$(PACKAGES_STAGE1)),$(eval $(call build-package,libraries/integer-simple,dist-install,1)))
-$(if $(filter integer-simple,$(PACKAGES_STAGE2)),$(eval $(call build-package,libraries/integer-simple,dist-install,2)))
--- GHC-7.8.2-modified/ghc-7.8.2/libraries/old-locale/ghc.mk
+++
@@ -1,5 +0,0 @@
-libraries/old-locale_PACKAGE = old-locale
-libraries/old-locale_dist-install_GROUP = libraries
-$(if $(filter old-locale,$(PACKAGES_STAGE0)),$(eval $(call build-package,libraries/old-locale,dist-boot,0)))
-$(if $(filter old-locale,$(PACKAGES_STAGE1)),$(eval $(call build-package,libraries/old-locale,dist-install,1)))
-$(if $(filter old-locale,$(PACKAGES_STAGE2)),$(eval $(call build-package,libraries/old-locale,dist-install,2)))
